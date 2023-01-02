56148824bceef7a1b6a016f2f3c8857ce46768d1
MilenaSilveira <120193252+MilenaSilveira@users.noreply.github.com>
2023-01-02 17:48:22 -0500

    Add answer and update styling

--- script.js
+++ script.js
@@ -1,73 +1,149 @@
 
 //Select all Elements
-const startButton = document.getElementById("start")
-const startQuiz = document.getElementById("quiz")
-const questionElement = document.getElementById("question")
-const timerElement = getElementById("timer")
-const scoreElement = getElementById("score")
-// const answerButton = document.getElementById("answer-btn")
+
+let startButton = document.getElementById("start-btn")
+const quizStart = document.getElementById("quiz")
+const titleElement = document.getElementById("title")
 const choiceA = document.getElementById("A")
 const choiceB = document.getElementById("B")
 const choiceC = document.getElementById("C")
 const choiceD = document.getElementById("D")
 
-questions[0].question
-questions[0].choiceA
-questions[0].choiceB
-questions[0].choiceC
-questions[0].choiceD
-questions[0].correct
+//Timer
 
-startButton.addEventListener("click", startQuiz)
-
-function startQuiz(){
-  startButton.style.display = "none";
-  timerElement = setInterval(100000);
-  renderQuestion();
-  startQuiz.style.display = "block";
+let time = 60;
+const counter = document.getElementById(`timer`);
+// setInterval(startTimer, 1000)
+let timeInterval;
+function startTimer() {
+  counter.innerHTML = `Timer: ${time}`;
+  timeInterval = setInterval(function () {
+    time--;
+    counter.innerHTML = `Timer: ${time}`;
+    if (time <= 0) {
+      clearInterval(timeInterval);
+      time = 0;
+      counter.innerHTML = `Timer: ${time}`;
+      scoreElement();
+    }
+  }, 1000);
 }
+
+//Create Questions
+
+let titleQuestion = document.getElementById("question")
+
+let questions = [
+  {
+    question: "What does HTML stand for?",
+    choiceA: "Hyperactive Markup Language",
+    choiceB: "Hover Tool Markup Language",
+    choiceC: "HyperText Markup Language",
+    choiceD: "Hyperlink Markup Lnguage",
+    correct: "C"
+
+  },
+
+  {
+    question: "What are the boolean values?",
+    choiceA: "Yes and No",
+    choiceB: "True and False",
+    choiceC: "Let and Const",
+    choiceD: "If and Else",
+    correct: "B"
+
+  },
+
+  {
+    question: "What is an Array?",
+    choiceA: "A collection of items, or data, stored in contiguous memory locations",
+    choiceB: "A text editor App",
+    choiceC: "An HTML element",
+    choiceD: "None of the above",
+    correct: "A"
+
+  },
+
+  {
+    question: "What are the JavaScript data types?",
+    choiceA: "Number, String, Boolean, Class and Array",
+    choiceB: "Number, String, Boolean, Object and Body",
+    choiceC: "Number, String, Boolean, Object and Undefined",
+    choiceD: "Number, Class, ID, Oject and String",
+    correct: "C"
+
+  },
+
+  {
+    question: "What does SQL stand for?",
+    choiceA: "Single Query Language",
+    choiceB: "Statement Query Language",
+    choiceC: "Stylesheet Query Language",
+    choiceD: "Structured Query Language",
+    correct: "D"
+
+  }
+]
+
+//Create Variables
 
 let lastQuestionIndex = questions.length- 1;
 let runningQuestionIndex = 0;
 
+// Render a Question
+
 function renderQuestion(){
   let q = questions[runningQuestionIndex];
-  question.innerHTML = "<h2>" + q.question + "</h2>";
+
+  titleQuestion.textContent= q.question
+  // titleElement.innerHTML = "<h2>" + q.question + "</h2>";
   choiceA.innerHTML = q.choiceA;
   choiceB.innerHTML = q.choiceB;
   choiceC.innerHTML = q.choiceC;
   choiceD.innerHTML = q.choiceD;
+ 
+}
+
+// let title = document.getElementsByClassName("hide")
+
+startButton.addEventListener("click", function startQuiz() {
+  // title.classList.add('hide')
+  startButton.classList.add('hide');
+  titleElement.classList.add('hide');
+  quizStart.classList.remove('hide');
+  renderQuestion();
+  startTimer();
+});
+
+
+
+//checkAnswer
+
+function checkAnswer(){
+ if (answer == questions [runningQuestionIndex].correct) {
+  scoreElement++;
+  correctAnswer();
+ }else{
+  wrongAnswer();
+}
+  
+ if(runningQuestionIndex < lastQuestionIndex) {
+  runningQuestionIndex++;
+  renderQuestion();
+ }else{
+  clearInterval("timer");
+  scoreElement();
+ }
 }
 
 function correctAnswer() {
-  document.getElementById(runningQuestionIndex).style.backgroundColor = "green";
+  document.getElementById(runningQuestionIndex).style.backgroundColor = "#0f0";
   
 }
 
 function wrongAnswer() {
-  document.getElementById(runningQuestionIndex).style.backgroundColor = "red";
+  document.getElementById(runningQuestionIndex).style.backgroundColor = "#f00";
   
-}
-
-let score = 0;
-
-function checkAnswer(answer){
-  if (questions[runningQuestionIndex].correct == answer){
-    score++;
-    correctAnswer();
-  }else{
-    wrongAnswer();
-  }
-  if(runningQuestionIndex < lastQuestionIndex) {
-    count = 0;
-    runningQuestionIndex++;
-    renderQuestion();
-  }else{
-    clearInterval("timer");
-  }
-  }
-  }
-  }
 }
 
 function scoreElement(){
@@ -76,210 +152,12 @@ function scoreElement(){
   
 }
 
-let questions = [
-  {
-    question: "What does HTML stand for?";
-    choiceA: "Hyperactive Markup Language";
-    choiceB: "Hover Tool Markup Language";
-    choiceC: "HyperText Markup Language";
-    choiceD: "Hyperlink Markup Lnguage";
-    correct: "C"
 
-  };
+// // // questions[0].question
+// // // questions[0].choiceA
+// // // questions[0].choiceB
+// // // questions[0].choiceC
+// // // questions[0].choiceD
+// // // questions[0].correct
 
-  {
-    question: "What does CSS stand for?";
-    choiceA: "Cats So Soft";
-    choiceB: "Hover Tool Markup Language";
-    choiceC: "HyperText Markup Language";
-    choiceD: "Hyperlink Markup Lnguage";
-    correct: "C"
 
-  };
-
-  {
-    question: "What does HTML stand for?";
-    choiceA: "Hyperactive Markup Language";
-    choiceB: "Hover Tool Markup Language";
-    choiceC: "HyperText Markup Language";
-    choiceD: "Hyperlink Markup Lnguage";
-    correct: "C"
-
-  };
-
-  {
-    question: "What does HTML stand for?";
-    choiceA: "Hyperactive Markup Language";
-    choiceB: "Hover Tool Markup Language";
-    choiceC: "HyperText Markup Language";
-    choiceD: "Hyperlink Markup Lnguage";
-    correct: "C"
-
-  };
-
-  {
-    question: "What does HTML stand for?";
-    choiceA: "Hyperactive Markup Language";
-    choiceB: "Hover Tool Markup Language";
-    choiceC: "HyperText Markup Language";
-    choiceD: "Hyperlink Markup Lnguage";
-    correct: "C"
-
-  }
-]
-
-
-// startButton.addEventListener("click", startQuiz)
-
-// function startQuiz() {
-//     console.log("Started")
-//     startQuestion.classList.add("hide")
-// }
-
-// function startQuestion() {
-//     questionElement.innerText = question.question
-//     question.answers.forEach(answer => {
-//         const button = document.createElement("button")
-//         button.innerText = answer.text
-//         button.classList.add("btn")
-//         if (answer.correct) {
-//             button.dataset.correct = answer.correct
-//          }
-//          button.addEventListener("click", selectAnswer)
-//          answerButton.appendChild(button)
-//     });
-// }
-
-
-// function selectAnswer {
-
-// }
-
-// const question = [
-//     {
-//         question: "What does HTML stand for? ",
-//         answers: [
-//             { text: "Hypertext Markup Language ", correct: true}
-//             { text: "How to apply Makeup"}
-//         ]
-//     }
-
-// ]
-
-
-
-
-
-
-// // Elements for buttons and containers pulled
-
-// const questionContainerElement = document.getElementById('question-section');
-// const questionElement = document.getElementById('question');
-// const answerButtonsElement = document.getElementById('answer-buttons');
-// const highscoresButton = document.getElementById('highscores-btn');
-// const startButton = document.getElementById('start-btn');
-// const nextButton = document.getElementById('next-btn');
-
-// //Timer Variables
-
-// const startingTime = 600;
-// let time = startingTime ;
-// const timerElement = document.getElementById('timer');
-
-// / Function for the timer
-// function updatecountdown(wrong,) {
-//   let timeleft = setInterval(() => {
-//     time--;
-//     if(timeleft == 0) {
-//       clearInterval(timeleft);
-//       window.alert('Sorry you have ran out of time.');
-//     }
-//   }, 1000);
-//   console.log(timeleft)
-
-//   timerElement.innerText = "Timer: " + timeleft;
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const startButton = document.getElementById("start-btn")
-// const startQuestion = document.getElementById("question-box")
-// const questionElement = document.getElementById("question")
-// const answerButton = document.getElementById("answer-btn")
-
-
-// startButton.addEventListener("click", startQuiz)
-
-// function startQuiz() {
-//     console.log("Started")
-//     startQuestion.classList.add("hide")
-// }
-
-// function startQuestion() {
-//     questionElement.innerText = question.question
-//     question.answers.forEach(answer => {
-//         const button = document.createElement("button")
-//         button.innerText = answer.text
-//         button.classList.add("btn")
-//         if (answer.correct) {
-//             button.dataset.correct = answer.correct
-//          }
-//          button.addEventListener("click", selectAnswer)
-//          answerButton.appendChild(button)
-//     });
-// }
-
-
-// function selectAnswer {
-
-// }
-
-// const question = [
-//     {
-//         question: "What does HTML stand for? ",
-//         answers: [
-//             { text: "Hypertext Markup Language ", correct: true}
-//             { text: "How to apply Makeup"}
-//         ]
-//     }
-
-// ]
